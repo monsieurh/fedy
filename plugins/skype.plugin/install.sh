4613bda40c68367888f7456ee77fe4da3f755fc1
ahub <ray.hubert@riseup.net>
2017-03-13 08:23:00 +0100

    Fixes install skypeforlinux

--- plugins/skype.plugin/install.sh
+++ plugins/skype.plugin/install.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-CACHEDIR="/var/cache/fedy/skype"
-
-mkdir -p "$CACHEDIR"
-cd "$CACHEDIR"
-
-URL="http://www.skype.com/go/getskype-linux-fc10"
-FILE="skype_linux.rpm"
-
+URL="/tmp/skypeforlinux-64-alpha.deb"
+FILE="https://go.skype.com/skypeforlinux-64-alpha.deb"
+wget -O  ${FILE}
 wget -c "$URL" -O "$FILE"
 
 if [[ ! -f "$FILE" ]]; then
 	exit 1
 fi
 
-dnf -y install gtk2-engines.i686
-dnf -y install $FILE
+dpkg -i ${FILE}
