4bc792fe57c2ed1ac4203af4cb54511979b3949f
monsieurh <ray.hubert@riseup.net>
2017-03-18 11:48:16 +0100

    Adds software-properties-common to spotify plugin since it's needed to add a PPA

--- plugins/spotify.plugin/install.sh
+++ plugins/spotify.plugin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-sudo add-apt-repository -y "deb http://repository.spotify.com stable non-free"
-sudo apt-key -y adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
-sudo apt -y update
-sudo apt -y install spotify-client
+apt install -y software-properties-common
+add-apt-repository -y "deb http://repository.spotify.com stable non-free"
+apt-key -y adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+apt -y update
+apt -y install spotify-client
