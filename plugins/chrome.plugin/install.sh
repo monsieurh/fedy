e45230d733a5bf61383622560faeaefd83423fdf
monsieurh <ray.hubert@riseup.net>
2017-03-18 11:49:47 +0100

    Should fix chrome (again)

--- plugins/chrome.plugin/install.sh
+++ plugins/chrome.plugin/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+dpkg -i --force-depends google-chrome-stable_current_amd64.deb
