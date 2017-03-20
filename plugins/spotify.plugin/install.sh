1caf48b007ffdf25a160d1f1960ca2dd30bfd8b3
Miguel Valdes <exlilote@gmail.com>
2017-03-20 07:42:18 -0500

    Added Albert, weather plugin and removed double software-common

--- plugins/spotify.plugin/install.sh
+++ plugins/spotify.plugin/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt install -y software-properties-common
 add-apt-repository -y "deb http://repository.spotify.com stable non-free"
 apt-key -y adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 apt -y update
