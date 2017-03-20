274f31be3a4aa341b64f0ea43cf538e1e7692225
Miguel Valdes <exlilote@gmail.com>
2017-03-20 08:49:13 -0500

    added common-prop for spotify

--- plugins/spotify.plugin/install.sh
+++ plugins/spotify.plugin/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install -y software-properties-common
 add-apt-repository -y "deb http://repository.spotify.com stable non-free"
 apt-key -y adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 apt -y update
